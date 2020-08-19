acb10bf7fbbceb71789909d72f3855189205b69e
Ioan Dicu <57108487+dixxii11@users.noreply.github.com>
2020-08-19 14:44:24 +0300

    Add humidity message

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('request')
 
+
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=0c93d926108af2f7ef52407993fee12f&query=' + latitude + ',' + longitude + '&units=f'
 
@@ -9,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback(undefined, 'Unable to find location')
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out. It feels like " + body.current.feelslike + " degrees out")
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out. It feels like " + body.current.feelslike + " degrees out. The humidity is " +body.current.humidity)
         }
     })
 }
