a79bff5f8c4bca0f2e143733405038a629d5a0bb
Ioan Dicu <57108487+dixxii11@users.noreply.github.com>
2020-08-19 14:14:55 +0300

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messgaeTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             messgaeOne.textContent = data.error
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Exspress config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -97,11 +98,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
-
-app.get('/weather', (req, res) => {
-    // All query string key/value pairs are on req.query
-    res.send('You provided "' + req.query.address + '" as the address.')
-    })
